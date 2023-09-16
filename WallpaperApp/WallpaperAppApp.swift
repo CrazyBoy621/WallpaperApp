1a9ef22bbace6f3f4c36dee377c9474fd6121d52
CrazyBoy <65382951+CrazyBoy621@users.noreply.github.com>
2023-09-16 18:01:43 +0500

    Initial Commit

---
+++ WallpaperApp/WallpaperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperAppApp.swift
+//  WallpaperApp
+//
+//  Created by Shohjahon Rakhmatov on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
